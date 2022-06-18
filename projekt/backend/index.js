b460e63bc18a19b32c309b2247400521435216af
Szymon Merski <merski70@gmail.com>
2022-06-18 15:09:25 +0200

    wszystko na raz zrobione

--- projekt/backend/index.js
+++ projekt/backend/index.js
@@ -20,8 +20,8 @@ const mongoose = require("mongoose");
 
 const client = redis.createClient({
   legacyMode: true,
-  host: "127.0.0.1",
-  port: 6379,
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || 6379,
 });
 
 client.hGet = util.promisify(client.hGet);
